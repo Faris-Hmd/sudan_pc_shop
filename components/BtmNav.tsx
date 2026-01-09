7c54086417f744d3d7ca36244fea9c3e5cd22a97
Faris-Hmd <88156110+Faris-Hmd@users.noreply.github.com>
2026-01-10 00:49:57 +0200

    Update BtmNav.tsx

--- components/BtmNav.tsx
+++ components/BtmNav.tsx
@@ -69,7 +69,7 @@ export default function BtmNav() {
   }
 
   return (
-    <nav className="fixed bottom-0 left-0 right-0 z-50 flex items-center justify-around border-t dark:border-slate-800 bg-white/90 dark:bg-slate-900/90 backdrop-blur-lg pb-safe md:hidden h-16 px-2">
+    <nav className="fixed bottom-0 left-0 right-0 z-50 flex items-center justify-around border-t dark:border-slate-800 bg-white/90 dark:bg-slate-900/90 pb-safe md:hidden h-16 px-2">
       {navItems.map((item) => {
         const Icon = item.icon;
         const isActive = item.href === "/" 
